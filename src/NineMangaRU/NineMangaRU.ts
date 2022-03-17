26464fd419492ca90e820a14be01e1eccfadab2f
pandeynmn <namanpandey23@gmail.com>
2022-03-17 08:36:53 -0700

    Source: Added Russian Ninemanga

---
+++ src/NineMangaRU/NineMangaRU.ts
@@ -0,0 +1,68 @@
+import { ContentRating,
+    LanguageCode,
+    MangaStatus,
+    SourceInfo,
+    TagType } from 'paperback-extensions-common'
+import { getExportVersion,
+    NineManga } from '../NineManga'
+
+const RU_DOMAIN = 'https://ru.ninemanga.com'
+
+export const NineMangaRUInfo: SourceInfo = {
+    version: getExportVersion('0.0.1'),
+    name: 'NineMangaRU',
+    description: 'Extension that pulls manga from ru.ninemanga.com',
+    author: 'NmN',
+    authorWebsite: 'http://github.com/pandyenmn',
+    icon: 'icon.png',
+    contentRating: ContentRating.EVERYONE,
+    language: LanguageCode.RUSSIAN,
+    websiteBaseURL: RU_DOMAIN,
+    sourceTags: [
+        {
+            text: 'Notifications',
+            type: TagType.GREEN,
+        },
+        {
+            text: 'Russian',
+            type: TagType.GREY,
+        },
+    ],
+}
+
+export class NineMangaRU extends NineManga {
+    baseUrl: string = RU_DOMAIN
+    languageCode: LanguageCode = LanguageCode.ITALIAN
+
+    genreTag = 'Genere(s)'
+    authorTag = 'Author(s)'
+    statusTag = 'Stato'
+
+    override parseStatus(str: string): MangaStatus {
+        let status = MangaStatus.UNKNOWN
+
+        switch (str.toLowerCase()) {
+            case 'in corso':
+                status = MangaStatus.ONGOING
+                break
+            case 'completato':
+                status = MangaStatus.COMPLETED
+                break
+        }
+        return status
+    }
+
+    protected override convertTime(timeAgo: string): Date {
+        let time: Date
+        let trimmed = Number((/\d*/.exec(timeAgo) ?? [])[0])
+        trimmed = trimmed == 0 && timeAgo.includes('a') ? 1 : trimmed
+        if (timeAgo.includes('mins') || timeAgo.includes('minutes') || timeAgo.includes('minute')) {
+            time = new Date(Date.now() - trimmed * 60000)
+        } else if (timeAgo.includes('ore') || timeAgo.includes('hour')) {
+            time = new Date(Date.now() - trimmed * 3600000)
+        } else {
+            time = new Date(timeAgo)
+        }
+        return time
+    }
+}
